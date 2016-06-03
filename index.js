a61f475be5fb5e764015e1c412bb5b4768bbe0d6
potato4d <mail@potato4d.me>
2016-06-03 16:47:45 +0900

    ファイルロード及び保存の動きの作成

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
+const remote = electron.remote
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,106 @@
+var fs = require('fs');
 var $ = require('jquery');
 var marked = require('marked');
+var nl2br = require('nl2br');
+
+var remote = require('electron').remote;
+var dialog = remote.dialog;
+var browserWindow = remote.BrowserWindow;
+
+var currentPath = "";
 
 $( _=> {
+
+	fs.readdir('.', function(err, files){
+		if (err) throw err;
+		var fileList = [];
+		files.filter(function(file){
+		    return fs.statSync(file).isFile() && /.*\.md$/.test(file); //絞り込み
+		}).forEach(function (file) {
+		    fileList.push(file);
+		});
+
+	    console.log(fileList);
+
+	    $.each(fileList, function() {
+	    	fs.readFile('./' + this, 'utf8', (err, text) => {
+	    		if (err) throw err;
+
+				$('.filetree-list').append(
+					'<li class="filetree-list-content">\
+						<b>' + this + '</b>\
+						<p class="filetree-list-content-body">\
+							' + nl2br(text, false) + '\
+						</p>\
+					</li>'
+				);
+			});
+	    });
+	});
+
+	function save(){
+
+		//　初期の入力エリアに設定されたテキストを保存しようとしたときは新規ファイルを作成する
+		if (currentPath == "") {
+			saveNewFile();
+			return;
+		}
+
+	    var win = browserWindow.getFocusedWindow();
+	 
+	    dialog.showMessageBox(win,
+	        {
+	            title: 'ファイルの上書き保存を行います。',
+	            type: 'info',
+	            buttons: ['OK', 'Cancel'],
+	            detail: '本当に保存しますか？'
+	        },
+	        // メッセージボックスが閉じられた後のコールバック関数
+	        function (respnse) {
+	            // OKボタン(ボタン配列の0番目がOK)
+	            if (respnse == 0) {
+	                var data = $(".editor-textarea").val();
+	                writeFile(currentPath, data);
+	            }
+	        }
+	    );
+	};
+
+	function saveNewFile() {
+
+		var win = browserWindow.getFocusedWindow();
+		dialog.showSaveDialog(
+			win,
+			// どんなダイアログを出すかを指定するプロパティ
+			{
+				properties: ['openFile'],
+				filters: [
+					{
+						name: 'Documents',
+						extensions: ['txt', 'text', 'html', 'js']
+					}
+				]
+			},
+			// セーブ用ダイアログが閉じられた後のコールバック関数
+			function (fileName) {
+				if (fileName) {
+					var data = $(".editor-textarea").val();
+					currentPath = fileName;
+					writeFile(currentPath, data);
+				}
+			}
+		);
+	}
+
+	function writeFile(path, data) {
+		fs.writeFile(path, data, function (error) {
+			if (error != null) {
+				alert('error : ' + error);
+				return;
+			}
+		});
+	}
+
 	marked.setOptions({
 		renderer: new marked.Renderer(),
 		gfm: true,
@@ -23,9 +122,9 @@ $( _=> {
 		event.preventDefault();
 		$('.filetree-list').prepend(
 			'<li class="filetree-list-content">\
-				<b>file' + parseInt(Math.random()*100) + '.md</b>\
+				<b>新規ファイル</b>\
 				<p>\
-					要約要約要約要約要約要約要約要約要約要約要約要約要約要約要約要約要約要約要約要約要約要約要約要約要約要約要約\
+					\
 				</p>\
 			</li>'
 		);
@@ -58,12 +157,28 @@ $( _=> {
 		$('.button-settings').addClass('active');
 	});
 
-	$*
+	$(".editor-textarea").keydown(function(event){
+		// Tab
+    	if (event.keyCode === 9) {
+        	event.preventDefault();
+        	var element = event.target;
+        	var val = element.value;
+        	var position = element.selectionStart;
+       		element.value = val.substr(0, position) + '\t' + val.substr(position, val.length);
+        	element.setSelectionRange(position + 1, position + 1);
+    	}
+	});
 
-	$(".editor-textarea").blur(function(event) {
+	$('.editor-textarea').on('blur keyup', function(event) {
+		// event.preventDefault();
 		var title = $(marked(($(this).val().replace(/\r\n|\r/g, "\n").split('\n'))[0]));
 		$(".file-title").text(title.text());
 		var md = marked($(this).val());
 		$(".editor-preview .editor-content-wrapper").html(md);
+	});
+
+	$(window).keydown(function(event){
+		if(event.keyCode !== 83) return;
+		if(event.shiftKey || event.metaKey) save();
 	});
 })
