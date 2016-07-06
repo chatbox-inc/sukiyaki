5783b5dade7a7f02480c5a82e081eb32c022cebf
Takuma Hanatani <mail@potato4d.me>
2016-07-06 14:57:21 +0900

    設定メニューをESLintに沿った形に修正

--- src/js/components/settings/index.js
+++ src/js/components/settings/index.js
@@ -6,14 +6,13 @@ const template = {
 			indent_width: "",
 			root_dir: "",
 			config: require("../../stores/config")
-		}
+		};
 	},
 
 	created: function (){
 		this.indent_type = this.config.indent_type;
 		this.indent_width = this.config.indent_width;
 		this.root_dir = this.config.root_dir;
-		console.log(this.indent_width);
 	},
 
 	methods: {
@@ -25,14 +24,14 @@ const template = {
 		},
 
 		selectPath: function() {
-			var remote = require('electron').remote;
+			var remote = require("electron").remote;
 			var dialog = remote.dialog;
 			var browserWindow = remote.BrowserWindow;
 			event.preventDefault();
 			var focusedWindow = browserWindow.getFocusedWindow();
 
 			dialog.showOpenDialog(focusedWindow, {
-				properties: ['openDirectory']
+				properties: ["openDirectory"]
 			}, (directories) => {
 				directories.forEach((directory) =>{
 					this.root_dir = directory;
