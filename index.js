1cd0105683c854018da864cfd8e4d5ce8595dc9e
Takuma Hanatani <mail@potato4d.me>
2016-07-06 15:39:20 +0900

    配布用設定の記述

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function createWindow () {
 
 	mainWindow.loadURL(`file://${__dirname}/index.html`)
 
-	mainWindow.webContents.openDevTools()
+	// mainWindow.webContents.openDevTools()
 
 	mainWindow.on('closed', function () {
 		mainWindow = null
--- src/js/main.js
+++ src/js/main.js
@@ -17,4 +17,4 @@ new Vue({
 	}
 });
 
-require("electron-connect").client.create();
+// require("electron-connect").client.create();
