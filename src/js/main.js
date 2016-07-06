407026a1af42d4a786b715d601dcd7eff7c3bd19
Takuma Hanatani <mail@potato4d.me>
2016-07-06 14:14:55 +0900

    main.jsをリファクタリング

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,12 @@
-const fs     = require('fs');
-const $      = require('jquery');
-const Vue    = require('vue');
+const Vue    = require("vue");
 const utility = require("./utility");
-const remote = require('electron').remote;
-const dialog = remote.dialog;
-const browserWindow = remote.BrowserWindow;
-const path = require("path");
 
 require("./components");
-var stores = require("./stores");
 var action = require("./services/action");
 
 utility.init();
 
-var app = new Vue({
+new Vue({
 	el: "#app",
 
 	methods: {
@@ -24,4 +17,4 @@ var app = new Vue({
 	}
 });
 
-require('electron-connect').client.create();
+require("electron-connect").client.create();
